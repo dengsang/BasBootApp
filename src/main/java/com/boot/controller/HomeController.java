27069d459971ad03cad9737fc54db8f3eb1d1be1
dengsang <dengsangt@gmail.com>
2018-05-22 18:05:47 +0300

    message

---
+++ src/main/java/com/boot/controller/HomeController.java
@@ -0,0 +1,16 @@
+package com.boot.controller;
+
+import  org.springframework.web.bind.annotation.RequestMapping;
+import  org.springframework.web.bind.annotation.RestController;
+
+
+
+
+@RestController
+public class HomeController {
+
+    @RequestMapping("/")
+    public String Home() {
+        return "Bas Boot, reporting to duty";
+    }
+}
